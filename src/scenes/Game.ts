03b8a63bb1f7bf2f1a494110a3873b3df16a7bf3
Renato Neto <rnato.netoo@gmail.com>
2023-06-04 22:14:48 -0300

    feat: character physics and camera scroll

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -2,8 +2,11 @@ import Phaser from 'phaser'
 
 import SceneKeys from '../consts/SceneKeys'
 import AnimationKeys from '../consts/AnimationKeys'
+import TextureKeys from '../consts/TextureKeys'
 
 export default class Game extends Phaser.Scene {
+  private background!: Phaser.GameObjects.TileSprite
+
   constructor() {
     super(SceneKeys.Game)
   }
@@ -15,9 +18,29 @@ export default class Game extends Phaser.Scene {
     // background
     const width = this.scale.width
     const height = this.scale.height
-    this.add.tileSprite(0, 0, width, height, 'background').setOrigin(0)
+    this.background = this.add.tileSprite(0, 0, width, height, TextureKeys.Background).setOrigin(0, 0).setScrollFactor(0, 0)
 
     // character
-    this.add.sprite(width * 0.5, height * 0.5, 'rocket-mouse', 'rocketmouse_fly01.png').play(AnimationKeys.RocketMouseRun)
+    const mouse = this.physics.add.sprite(
+      width * 0.5,
+      height -30,
+      TextureKeys.RocketMouse,
+      'rocketmouse_fly01.png'
+    )
+      .setOrigin(0.5, 1)
+      .play(AnimationKeys.RocketMouseRun)
+
+    const body = mouse.body as Phaser.Physics.Arcade.Body
+    body.setCollideWorldBounds(true)
+    body.setVelocityX(200)
+    this.physics.world.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height - 30)
+
+    // camera
+    this.cameras.main.startFollow(mouse)
+    this.cameras.main.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height)
+  }
+
+  update(t: number, dt: number) {
+    this.background.setTilePosition(this.cameras.main.scrollX)
   }
 }
