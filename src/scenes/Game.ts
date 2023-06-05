e2364419eecb7f356c0d819eff741a52e8f0b3b3
Renato Neto <rnato.netoo@gmail.com>
2023-06-04 22:37:16 -0300

    add windows in the scene

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -7,6 +7,8 @@ import TextureKeys from '../consts/TextureKeys'
 export default class Game extends Phaser.Scene {
   private background!: Phaser.GameObjects.TileSprite
   private mouseHole!: Phaser.GameObjects.Image
+  private window1!: Phaser.GameObjects.Image
+  private window2!: Phaser.GameObjects.Image
 
   constructor() {
     super(SceneKeys.Game)
@@ -25,6 +27,16 @@ export default class Game extends Phaser.Scene {
       Phaser.Math.Between(900, 1500),
       501,
       TextureKeys.MouseHole
+    )
+    this.window1 = this.add.image(
+      Phaser.Math.Between(900, 1300),
+      200,
+      TextureKeys.Window1
+    )
+    this.window2 = this.add.image(
+      Phaser.Math.Between(1600, 2000),
+      200,
+      TextureKeys.Window2
     )
 
     // character
@@ -49,6 +61,7 @@ export default class Game extends Phaser.Scene {
 
   update(t: number, dt: number) {
     this.wrapMouseHole()
+    this.wrapWindows()
     this.background.setTilePosition(this.cameras.main.scrollX)
   }
 
@@ -60,6 +73,27 @@ export default class Game extends Phaser.Scene {
       this.mouseHole.x = Phaser.Math.Between(
         rightEdge + 100,
         rightEdge + 1000
+      )
+    }
+  }
+
+  private wrapWindows() {
+    const scrollX = this.cameras.main.scrollX
+    const rightEdge = scrollX + this.scale.width
+
+    let width = this.window1.width * 2
+    if (this.window1.x + width < scrollX) {
+      this.window1.x = Phaser.Math.Between(
+        rightEdge + width,
+        rightEdge + width + 800
+      )
+    }
+
+    width = this.window2.width
+    if (this.window2.x + width < scrollX) {
+      this.window2.x = Phaser.Math.Between(
+        this.window1.x + width,
+        this.window1.x + width + 800
       )
     }
   }
