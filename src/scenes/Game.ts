811ad9684aac3bf6c5e8eab8a7ba74f3e14f9259
Renato Neto <rnato.netoo@gmail.com>
2023-06-04 23:04:13 -0300

    add bookcases in the scene

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -9,6 +9,10 @@ export default class Game extends Phaser.Scene {
   private mouseHole!: Phaser.GameObjects.Image
   private window1!: Phaser.GameObjects.Image
   private window2!: Phaser.GameObjects.Image
+  private bookcase1!: Phaser.GameObjects.Image
+  private bookcase2!: Phaser.GameObjects.Image
+  private bookcases: Phaser.GameObjects.Image[] = []
+  private windows: Phaser.GameObjects.Image[] = []
 
   constructor() {
     super(SceneKeys.Game)
@@ -21,6 +25,7 @@ export default class Game extends Phaser.Scene {
     // background
     const width = this.scale.width
     const height = this.scale.height
+
     this.background = this.add.tileSprite(0, 0, width, height, TextureKeys.Background).setOrigin(0, 0).setScrollFactor(0, 0)
 
     this.mouseHole = this.add.image(
@@ -28,6 +33,7 @@ export default class Game extends Phaser.Scene {
       501,
       TextureKeys.MouseHole
     )
+
     this.window1 = this.add.image(
       Phaser.Math.Between(900, 1300),
       200,
@@ -38,6 +44,19 @@ export default class Game extends Phaser.Scene {
       200,
       TextureKeys.Window2
     )
+    this.windows = [this.window1, this.window2]
+
+    this.bookcase1 = this.add.image(
+      Phaser.Math.Between(2200, 2700),
+      580,
+      TextureKeys.Bookcase1
+    ).setOrigin(0.5, 1)
+    this.bookcase2 = this.add.image(
+      Phaser.Math.Between(2900, 3400),
+      580,
+      TextureKeys.Bookcase2
+    ).setOrigin(0.5, 1)
+    this.bookcases = [this.bookcase1, this.bookcase2]
 
     // character
     const mouse = this.physics.add.sprite(
@@ -51,7 +70,7 @@ export default class Game extends Phaser.Scene {
 
     const body = mouse.body as Phaser.Physics.Arcade.Body
     body.setCollideWorldBounds(true)
-    body.setVelocityX(200)
+    body.setVelocityX(500)
     this.physics.world.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height - 30)
 
     // camera
@@ -62,6 +81,7 @@ export default class Game extends Phaser.Scene {
   update(t: number, dt: number) {
     this.wrapMouseHole()
     this.wrapWindows()
+    this.wrapBookcases()
     this.background.setTilePosition(this.cameras.main.scrollX)
   }
 
@@ -87,6 +107,10 @@ export default class Game extends Phaser.Scene {
         rightEdge + width,
         rightEdge + width + 800
       )
+      const overlap = this.bookcases.find(bc => {
+        return Math.abs(this.window1.x - bc.x) <= this.window1.width
+      })
+      this.window1.visible = !overlap
     }
 
     width = this.window2.width
@@ -95,6 +119,39 @@ export default class Game extends Phaser.Scene {
         this.window1.x + width,
         this.window1.x + width + 800
       )
+      const overlap = this.bookcases.find(bc => {
+        return Math.abs(this.window2.x - bc.x) <= this.window2.width
+      })
+      this.window2.visible = !overlap
+    }
+  }
+
+  private wrapBookcases() {
+    const scrollX = this.cameras.main.scrollX
+    const rightEdge = scrollX + this.scale.width
+
+    let width = this.bookcase1.width * 2
+    if (this.bookcase1.x + width < scrollX) {
+      this.bookcase1.x = Phaser.Math.Between(
+        rightEdge + width,
+        rightEdge + width + 800
+      )
+      const overlap = this.windows.find(win => {
+        return Math.abs(this.bookcase1.x - win.x) <= win.width
+      })
+      this.bookcase1.visible = !overlap
+    }
+
+    width = this.bookcase2.width
+    if (this.bookcase2.x + width < scrollX) {
+      this.bookcase2.x = Phaser.Math.Between(
+        this.bookcase1.x + width,
+        this.bookcase1.x + width + 800
+      )
+      const overlap = this.windows.find(win => {
+        return Math.abs(this.bookcase2.x - win.x) <= win.width
+      })
+      this.bookcase2.visible = !overlap
     }
   }
 }
